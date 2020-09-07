e8326cfb7fe88f44e7c14edd2d7a9949dfbadc8a
Nic McPhee <mcphee@morris.umn.edu>
2020-09-07 13:16:57 -0500

    Commit initial version
    This works, but is pretty ugly. Some things to clean up:
    
    * Set up environment variables or similar for the personal access token (currently in my 1Password and as a GitHub Action secret).
    * Clean up `package.json`. Current there's an `octokit` dependency that is essentially nonsense, and I don't think I use `@octokit/auth-app`. 
    * Turn this into a script with a command line argument so I can call it with an organization name and it will archive all unarchived repos in that organization.
    * Remove the various `console.log()` statements and replace them with some more coherent output (or logging).

---
+++ src/archive-github-org.js
@@ -0,0 +1,73 @@
+// import { request } from "@octokit/request"
+const { request } = require("@octokit/request");
+const { createAppAuth } = require("@octokit/auth-app");
+
+const authHeaders = request.defaults({
+  headers: {
+    authorization: myToken,
+  },
+});
+
+async function getAllUnarchivedOrgRepos(org) {
+  try {
+    const numRepos = await authHeaders('GET /orgs/:org', {
+      org: org
+    }).then(result => {
+      return result.data.public_repos;
+    });
+    console.log(`There are a total of ${numRepos} repos in this organization`);
+    let unarchivedRepos = [];
+    for (let i=0; i<numRepos/100; ++i) {
+      const result = await authHeaders('GET /orgs/:org/repos', {
+          org: org,
+          page: i+1,
+          per_page: 100,
+        })
+      unarchivedRepos = unarchivedRepos.concat(result.data.filter(repo => {
+        return !repo.archived;
+      }));
+    }
+    console.log(`And ${unarchivedRepos.length} of them were unarchived.`);
+    return unarchivedRepos;
+  } catch(e) {
+    console.error(e)
+  }
+}
+
+async function archiveSingleRepo(org, repoName) {
+  try {
+    const result = await authHeaders('PATCH /repos/:org/:repo', {
+      org: org,
+      repo: repoName,
+      archived: true
+    });
+    if (result.status == 200) {
+      console.log(`Successfully archived ${repoName}.`);
+      return repoName;
+    } else {
+      console.error(`Error archiving ${repoName}:`);
+      console.error(result);
+    }
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+async function archiveAllOrgRepos(org) {
+  const repoList = await getAllUnarchivedOrgRepos(org);
+  const repoNames = repoList.map(repo => {
+    return repo.name;
+  })
+  console.log(repoNames);
+  const archivedNames = repoNames.map(repoName => {
+    const archivedName = archiveSingleRepo(org, repoName);
+    return archivedName;
+  });
+  return archivedNames;
+}
+
+const archivedRepos = archiveAllOrgRepos('UMM-CSci-3403-Fall-2018');
+archivedRepos.then(repoNames => {
+  console.log(repoNames);
+})
+
